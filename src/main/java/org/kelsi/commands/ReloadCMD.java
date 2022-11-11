93918a7a20ff2513b036857562644ea15222c464
Leanfe <l.kapitman@gmail.com>
2022-11-11 17:13:33 +0200

    Update and rename reloadCMD.java to ReloadCMD.java

--- src/main/java/org/kelsi/commands/ReloadCMD.java
+++ src/main/java/org/kelsi/commands/ReloadCMD.java
@@ -8,20 +8,27 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
-public class reloadCMD implements CommandExecutor {
+public class ReloadCMD implements CommandExecutor {
 
     Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WaterDead");
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-
+        if (!(sender instanceof Player)) {
+            plugin.reloadConfig();
+            return true;
+        }
+        
         Player player = (Player)sender;
 
         if (player.hasPermission("wd.reload")) {
             plugin.reloadConfig();
             plugin.saveConfig();
             player.sendMessage("Plugin has reloaded!");
+            return true;
         }
+        
+        player.sendMessage("[ERROR] ReloadCMD has crashed!");
         return false;
     }
 }
